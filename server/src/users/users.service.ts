4c347cc4ab44800ca82e580b5975c8e9703fdfb4
sammy6378 <samuel202mwangi@gmail.com>
2025-05-28 19:49:14 +0300

    Refactor UsersService to implement user repository methods and improve error handling

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,28 +1,72 @@
-import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return createUserDto;
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return await this.userRepository
+      .save(createUserDto)
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => {
+        console.error('Error creating user:', err);
+        throw new Error('Failed to create user');
+      });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(full_name?: string) {
+    if (full_name) {
+      return await this.userRepository.find({
+        where: { full_name },
+      });
+    }
+    return await this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User | string> {
+    return await this.userRepository
+      .findOneBy({ id })
+      .then((user) => {
+        if (!user) {
+          return `User with id ${id} not found`;
+        }
+        return user;
+      })
+      .catch((err) => {
+        console.error('Error finding user', err);
+        throw new Error('user not found');
+      });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    console.log(updateUserDto);
-    return `User with id #${id} was updated`;
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User | string> {
+    await this.userRepository.update(id, updateUserDto);
+    return this.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    return await this.userRepository
+      .delete(id)
+      .then((res) => {
+        if (res.affected === 0) {
+          return `User with id ${id} not found`;
+        }
+        return `User with id ${id} deleted successfully`;
+      })
+      .catch((err) => {
+        console.error('Error deleting user:', err);
+        throw new Error('Failed to delete user');
+      });
   }
 }
