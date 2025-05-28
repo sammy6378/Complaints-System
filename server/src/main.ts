272a2633c3b47ee2404313a928313d71ff2dbd9c
sammy6378 <samuel202mwangi@gmail.com>
2025-05-28 19:48:13 +0300

    Refactor main.ts to retrieve PORT configuration using ConfigService instead of process.env

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import 'dotenv/config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   // create a NestJS application instance
@@ -14,7 +15,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   // port configuration
-  const PORT = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+  const PORT = configService.getOrThrow<number>('PORT');
+  // const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 
   Logger.log(`Server is running on http://localhost:${PORT}`);
