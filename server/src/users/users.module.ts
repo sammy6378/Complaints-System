d3e74bedd2b7f3d27c82a6835cf7376bcfe2b595
sammy6378 <samuel202mwangi@gmail.com>
2025-05-28 19:48:59 +0300

    Refactor UsersModule to include TypeOrmModule for User entity and DatabaseModule imports

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
