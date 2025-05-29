06a3fb3ee9cfac23c09ff65082a1a63362690133
sammy6378 <samuel202mwangi@gmail.com>
2025-05-29 21:48:31 +0300

    Add imports for DatabaseModule and TypeOrmModule in UserLogsModule

--- server/src/user-logs/user-logs.module.ts
+++ server/src/user-logs/user-logs.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserLogsService } from './user-logs.service';
 import { UserLogsController } from './user-logs.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserLog } from './entities/user-log.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([UserLog, User])],
   controllers: [UserLogsController],
   providers: [UserLogsService],
 })
