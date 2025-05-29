7d33cf7df4a64ffe453a70b9e3891586f2d7d5b9
sammy6378 <samuel202mwangi@gmail.com>
2025-05-29 21:49:17 +0300

    Add UserLog entity with properties for logging user actions and relationships

--- server/src/user-logs/entities/user-log.entity.ts
+++ server/src/user-logs/entities/user-log.entity.ts
@@ -1,1 +1,49 @@
-export class UserLog {}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+import { LoginStatus } from '../dto/create-user-log.dto';
+import { User } from 'src/users/entities/user.entity';
+
+@Entity('user_logs')
+export class UserLog {
+  @PrimaryGeneratedColumn()
+  log_id: number;
+
+  @Column({ type: 'enum', enum: LoginStatus, default: LoginStatus.SUCCESS })
+  login_status: LoginStatus;
+
+  @Column()
+  action: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+
+  //   relatioships
+  // user and user-logs [1 to * relationships]
+  @ManyToOne(() => User, (user) => user.userLogs, {
+    eager: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  user: Relation<User>;
+}
+
+// note
+// 💡 Best Practices
+// Always start with the ManyToOne side – it owns the relation. - has the foreign key.
+
+// The OneToMany side just references back using the property name in the owning side.
+
+// Use onDelete: 'CASCADE' if you want logs to be deleted when a user is deleted.
