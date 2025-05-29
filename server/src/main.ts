0c16e14a37c9946fa16b971529387d37ec35c9ac
sammy6378 <samuel202mwangi@gmail.com>
2025-05-29 14:17:45 +0300

    Enhance main.ts to configure logger levels and register global exception filter

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import 'dotenv/config';
 import { ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from './http-exceptions.filter';
 
 async function bootstrap() {
   // create a NestJS application instance
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'], // configure logger levels
+  });
 
   // add global validation pipe
   app.useGlobalPipes(new ValidationPipe());
+
+  // register global exception filter
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   // add global prefix for API routes
   app.setGlobalPrefix('api');
