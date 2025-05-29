b86f15045cc40c59e1022df85a6581a0443d9976
sammy6378 <samuel202mwangi@gmail.com>
2025-05-29 21:48:51 +0300

    Refactor UserLogsService to implement user log creation, retrieval, update, and deletion with proper error handling and response formatting

--- server/src/user-logs/user-logs.service.ts
+++ server/src/user-logs/user-logs.service.ts
@@ -1,26 +1,81 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserLogDto } from './dto/create-user-log.dto';
 import { UpdateUserLogDto } from './dto/update-user-log.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserLog } from './entities/user-log.entity';
+import { Repository } from 'typeorm';
+import { ApiResponse, createResponse } from 'src/utils/responseHandler';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class UserLogsService {
-  create(createUserLogDto: CreateUserLogDto) {
-    return 'This action adds a new userLog';
+  constructor(
+    @InjectRepository(UserLog) private logsRepository: Repository<UserLog>,
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(
+    createUserLogDto: CreateUserLogDto,
+  ): Promise<ApiResponse<UserLog>> {
+    const user = await this.userRepository.findOneBy({
+      id: createUserLogDto.userId,
+    });
+    if (!user) {
+      throw new Error(`User with id ${createUserLogDto.userId} not found`);
+    }
+    const userLog = this.logsRepository.create({
+      ...createUserLogDto,
+      user: user,
+    });
+
+    return await this.logsRepository
+      .save(userLog)
+      .then((log) => {
+        return createResponse(log, 'User log created successfully');
+      })
+      .catch((err) => {
+        console.error('Error creating user log:', err);
+        throw new Error('Failed to create user log');
+      });
   }
 
-  findAll() {
-    return `This action returns all userLogs`;
+  async findAll() {
+    return await this.logsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} userLog`;
+  async findOne(id: number): Promise<ApiResponse<UserLog> | string> {
+    return this.logsRepository
+      .findOneBy({ log_id: id })
+      .then((log) => {
+        if (!log) {
+          return `User log with id ${id} not found`;
+        }
+        return createResponse(log, 'User log found successfully');
+      })
+      .catch((err) => {
+        console.error('Error finding user log', err);
+        throw new Error('User log not found');
+      });
   }
 
-  update(id: number, updateUserLogDto: UpdateUserLogDto) {
-    return `This action updates a #${id} userLog`;
+  async update(
+    id: number,
+    updateUserLogDto: UpdateUserLogDto,
+  ): Promise<ApiResponse<UserLog> | string> {
+    await this.logsRepository.update(id, updateUserLogDto).then((log) => {
+      if (!log.affected) {
+        throw new Error(`User log with id ${id} not found`);
+      }
+    });
+    return await this.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} userLog`;
+  async remove(id: number) {
+    return await this.logsRepository.delete(id).then((res) => {
+      if (res.affected === 0) {
+        throw new Error(`User log with id ${id} not found`);
+      }
+      return createResponse(res, 'User log deleted successfully');
+    });
   }
 }
