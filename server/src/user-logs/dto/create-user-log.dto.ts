bce54b71176995cbd087a94706730e4bddf212ed
sammy6378 <samuel202mwangi@gmail.com>
2025-05-29 21:49:01 +0300

    Add CreateUserLogDto with validation and enum for login status

--- server/src/user-logs/dto/create-user-log.dto.ts
+++ server/src/user-logs/dto/create-user-log.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserLogDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export enum LoginStatus {
+  SUCCESS = 'success',
+  FAILURE = 'failure',
+}
+export class CreateUserLogDto {
+  @IsNotEmpty()
+  @IsNumber()
+  userId: number;
+
+  @IsEnum(LoginStatus)
+  login_status: LoginStatus;
+
+  @IsString()
+  action: string;
+}
