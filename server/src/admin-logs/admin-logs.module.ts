927cfd776f08247e700a7a3d05708399c98058cf
sammy6378 <samuel202mwangi@gmail.com>
2025-06-02 14:13:45 +0300

    Add imports for DatabaseModule and TypeOrmModule in AdminLogsModule

--- server/src/admin-logs/admin-logs.module.ts
+++ server/src/admin-logs/admin-logs.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminLogsService } from './admin-logs.service';
 import { AdminLogsController } from './admin-logs.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminLog } from './entities/admin-log.entity';
+import { Admin } from 'src/admins/entities/admin.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([AdminLog, Admin])],
   controllers: [AdminLogsController],
   providers: [AdminLogsService],
 })
