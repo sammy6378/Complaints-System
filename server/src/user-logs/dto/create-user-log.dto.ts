ed349d4d468929330c44216da758dc85ee0faf79
sammy6378 <samuel202mwangi@gmail.com>
2025-06-02 14:16:52 +0300

    Refactor CreateUserLogDto to update userId type from number to string and rename login_status to loginStatus

--- server/src/user-logs/dto/create-user-log.dto.ts
+++ server/src/user-logs/dto/create-user-log.dto.ts
@@ -1,17 +1,16 @@
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsEnum, IsString } from 'class-validator';
 
 export enum LoginStatus {
   SUCCESS = 'success',
   FAILURE = 'failure',
 }
 export class CreateUserLogDto {
-  @IsNotEmpty()
-  @IsNumber()
-  userId: number;
-
   @IsEnum(LoginStatus)
-  login_status: LoginStatus;
+  loginStatus: LoginStatus;
 
   @IsString()
   action: string;
+
+  @IsString()
+  userId: string;
 }
