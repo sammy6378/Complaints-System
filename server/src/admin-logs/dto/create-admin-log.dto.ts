7413282bfa75318f2664e011d7aeaeb25c21c664
sammy6378 <samuel202mwangi@gmail.com>
2025-06-02 14:14:10 +0300

    Add CreateAdminLogDto with validation decorators for login time, status, and admin ID

--- server/src/admin-logs/dto/create-admin-log.dto.ts
+++ server/src/admin-logs/dto/create-admin-log.dto.ts
@@ -1,1 +1,13 @@
-export class CreateAdminLogDto {}
+import { IsDateString, IsEnum, IsString } from 'class-validator';
+import { LoginStatus } from 'src/user-logs/dto/create-user-log.dto';
+
+export class CreateAdminLogDto {
+  @IsDateString()
+  loginTime: Date;
+
+  @IsEnum(LoginStatus)
+  loginStatus: LoginStatus;
+
+  @IsString()
+  adminId: string;
+}
