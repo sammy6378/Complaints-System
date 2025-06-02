c55863d7bef363b63a0ebd5677a6e4dd943cd2f9
sammy6378 <samuel202mwangi@gmail.com>
2025-06-02 14:15:02 +0300

    Add CreateAdminDto with validation decorators for username, fullName, email, and password

--- server/src/admins/dto/create-admin.dto.ts
+++ server/src/admins/dto/create-admin.dto.ts
@@ -1,1 +1,24 @@
-export class CreateAdminDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateAdminDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword()
+  password: string;
+}
