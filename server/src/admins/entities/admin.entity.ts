23d38b2871a2b99a3db6641f1390644f4aa8d714
sammy6378 <samuel202mwangi@gmail.com>
2025-06-02 14:15:13 +0300

    Add Admin entity with properties for adminId, username, fullName, email, password, created_at, updated_at, and relationship to AdminLog

--- server/src/admins/entities/admin.entity.ts
+++ server/src/admins/entities/admin.entity.ts
@@ -1,1 +1,42 @@
-export class Admin {}
+import { AdminLog } from 'src/admin-logs/entities/admin-log.entity';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+
+@Entity('admins')
+export class Admin {
+  @PrimaryGeneratedColumn('uuid')
+  adminId: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  fullName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+
+  @OneToMany(() => AdminLog, (adminLog) => adminLog.admin, {
+    cascade: true,
+    eager: true,
+    onDelete: 'CASCADE',
+  })
+  adminLogs: Relation<AdminLog[]>;
+}
