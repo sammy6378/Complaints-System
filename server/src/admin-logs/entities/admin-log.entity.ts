58bbf8b836fa6234074541282d4e39ec9ff2680e
sammy6378 <samuel202mwangi@gmail.com>
2025-06-02 14:14:21 +0300

    Add AdminLog entity with properties for log ID, login status, login time, and timestamps

--- server/src/admin-logs/entities/admin-log.entity.ts
+++ server/src/admin-logs/entities/admin-log.entity.ts
@@ -1,1 +1,36 @@
-export class AdminLog {}
+import { Admin } from 'src/admins/entities/admin.entity';
+import { LoginStatus } from 'src/user-logs/dto/create-user-log.dto';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+
+@Entity('admin_logs')
+export class AdminLog {
+  @PrimaryGeneratedColumn('uuid')
+  logId: string;
+
+  @Column({ type: 'enum', enum: LoginStatus, default: LoginStatus.SUCCESS })
+  loginStatus: LoginStatus;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  LoginTime: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+
+  @ManyToOne(() => Admin, (admin) => admin.adminLogs)
+  @JoinColumn()
+  admin: Relation<Admin>;
+}
