4a09991b80461e7eebc2796b9eccb5ec7dbfbf3a
sammy6378 <samuel202mwangi@gmail.com>
2025-06-03 20:07:59 +0300

    Implement Category entity with fields for category_id, category_name, description, created_at, updated_at, and subcategories relationship

--- server/src/categories/entities/category.entity.ts
+++ server/src/categories/entities/category.entity.ts
@@ -1,1 +1,26 @@
-export class Category {}
+import { Subcategory } from 'src/subcategories/entities/subcategory.entity';
+import { Column, OneToMany, PrimaryGeneratedColumn, Relation } from 'typeorm';
+
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  category_id: string;
+
+  @Column()
+  category_name: string;
+
+  @Column({ type: 'text' })
+  decription: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+
+  @OneToMany(() => Subcategory, (subCategory) => subCategory.category)
+  subCategories: Relation<Subcategory[]>;
+}
