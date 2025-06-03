1ada6807fd98aee4f87d431aa4f706b9c9281c52
sammy6378 <samuel202mwangi@gmail.com>
2025-06-03 20:06:49 +0300

    Refactor CategoriesModule to include TypeOrmModule and import Category and Subcategory entities

--- server/src/categories/categories.module.ts
+++ server/src/categories/categories.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Subcategory } from 'src/subcategories/entities/subcategory.entity';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Category, Subcategory])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
