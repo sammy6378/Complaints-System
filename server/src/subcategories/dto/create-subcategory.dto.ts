bb9d90e56f127d4cb0e265614ad32d4d499a5e02
sammy6378 <samuel202mwangi@gmail.com>
2025-06-03 20:21:13 +0300

    Add CreateSubcategoryDto with validation for subcategoryName, description, and categoryId fields

--- server/src/subcategories/dto/create-subcategory.dto.ts
+++ server/src/subcategories/dto/create-subcategory.dto.ts
@@ -1,1 +1,12 @@
-export class CreateSubcategoryDto {}
+import { IsString } from 'class-validator';
+
+export class CreateSubcategoryDto {
+  @IsString()
+  subcategoryName: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  categoryId: string;
+}
