244b88c678660ca8f418097d44fc4fbf815c7322
sammy6378 <samuel202mwangi@gmail.com>
2025-06-03 20:21:27 +0300

    Refactor Subcategory entity to improve code clarity and maintainability

--- server/src/subcategories/entities/subcategory.entity.ts
+++ server/src/subcategories/entities/subcategory.entity.ts
@@ -1,1 +1,36 @@
-export class Subcategory {}
+import { Category } from 'src/categories/entities/category.entity';
+import {
+  Column,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
+
+export class Subcategory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  subcategoryName: string;
+
+  @Column({ type: 'text', nullable: false })
+  description: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+
+  @ManyToOne(() => Category, (category) => category.subCategories, {
+    onDelete: 'CASCADE',
+    eager: true,
+  })
+  @JoinColumn()
+  category: Relation<Category>;
+}
