16b3ee619a942fbb59d4296ea127e0a98edbb4f4
sammy6378 <samuel202mwangi@gmail.com>
2025-06-04 11:57:03 +0300

    Add CreateComplaintDto with validation and enums for priority and status

--- server/src/complaints/dto/create-complaint.dto.ts
+++ server/src/complaints/dto/create-complaint.dto.ts
@@ -1,1 +1,41 @@
-export class CreateComplaintDto {}
+import { IsEnum, IsString } from 'class-validator';
+
+export enum complaint_priority {
+  Low = 'Low',
+  Medium = 'Medium',
+  High = 'High',
+}
+
+export enum complaint_status {
+  Pending = 'Pending',
+  UnderReview = 'Under Review',
+  Resolved = 'Resolved',
+  Closed = 'Closed',
+  Rejected = 'Rejected',
+  Reopened = 'Reopened',
+}
+export class CreateComplaintDto {
+  @IsString()
+  complaint_title: string;
+
+  @IsString()
+  complaint_description: string;
+
+  @IsEnum(complaint_status)
+  complaint_status: complaint_status;
+
+  @IsEnum(complaint_priority)
+  priority: complaint_priority;
+
+  @IsString()
+  userId: string;
+
+  @IsString()
+  categoryId: string;
+
+  @IsString()
+  subcategoryId: string;
+
+  @IsString()
+  stateId: string;
+}
