0e8e3206d4df3fdd0bccd0ff1c7c8437d7fda8ad
sammy6378 <samuel202mwangi@gmail.com>
2025-06-04 11:57:38 +0300

    Add imports for DatabaseModule and TypeOrmModule in StatesModule for data handling

--- server/src/states/states.module.ts
+++ server/src/states/states.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StatesService } from './states.service';
 import { StatesController } from './states.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { State } from './entities/state.entity';
+import { Complaint } from 'src/complaints/entities/complaint.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([State, Complaint])],
   controllers: [StatesController],
   providers: [StatesService],
 })
