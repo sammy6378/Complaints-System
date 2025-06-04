193d88cea5b8497205d182ed1e2925c62f260e75
sammy6378 <samuel202mwangi@gmail.com>
2025-06-04 11:56:35 +0300

    Add imports for DatabaseModule and TypeOrmModule in ComplaintsModule for improved data handling

--- server/src/complaints/complaints.module.ts
+++ server/src/complaints/complaints.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ComplaintsService } from './complaints.service';
 import { ComplaintsController } from './complaints.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Complaint } from './entities/complaint.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Category } from 'src/categories/entities/category.entity';
+import { Subcategory } from 'src/subcategories/entities/subcategory.entity';
+import { State } from 'src/states/entities/state.entity';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    TypeOrmModule.forFeature([Complaint, User, Category, Subcategory, State]),
+  ],
   controllers: [ComplaintsController],
   providers: [ComplaintsService],
 })
