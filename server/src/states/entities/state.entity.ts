b5903a1c4a20f92f7a27fb23094c4cdfb9ce684c
sammy6378 <samuel202mwangi@gmail.com>
2025-06-04 11:58:11 +0300

    Add State entity with properties for state management and relationships to Complaint

--- server/src/states/entities/state.entity.ts
+++ server/src/states/entities/state.entity.ts
@@ -1,1 +1,26 @@
-export class State {}
+import { Complaint } from 'src/complaints/entities/complaint.entity';
+import { Column, OneToMany, PrimaryGeneratedColumn, Relation } from 'typeorm';
+
+export class State {
+  @PrimaryGeneratedColumn('uuid')
+  state_id: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  state_name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  description: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+
+  @OneToMany(() => Complaint, (complaint) => complaint.state)
+  complaints: Relation<Complaint[]>;
+}
