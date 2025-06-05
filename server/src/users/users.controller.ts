ed34476b1405bd8858a4b937cc0deaabed94cca7
sammy6378 <samuel202mwangi@gmail.com>
2025-06-05 12:19:21 +0300

    Refactor UsersController to remove unused Query parameter and add Public decorator for create method

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -6,24 +6,28 @@ import {
   Patch,
   Param,
   Delete,
-  Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('users')
+@UseInterceptors(CacheInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Public()
   @Post('create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll(@Query('full_name') full_name?: string) {
-    return this.usersService.findAll(full_name);
+  findAll() {
+    return this.usersService.findAll();
   }
 
   @Get(':id')
