21a585ead08307799086d85b522a753b4952167c
sammy6378 <samuel202mwangi@gmail.com>
2025-06-05 14:39:25 +0300

    Add AppController to serve welcome message and API documentation links

---
+++ server/src/app.controller.ts
@@ -0,0 +1,15 @@
+// src/app.controller.ts
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHome(@Res() res: Response) {
+    res.send(`
+      <h1>Welcome to the Complaints System API</h1>
+      <p>Visit <a href="/api/reference">Documentation</a> for API documentation.</p>
+      <p>Visit <a href="https://complaints-system-production.up.railway.app">Live</a> Deployed site</p>
+    `);
+  }
+}
