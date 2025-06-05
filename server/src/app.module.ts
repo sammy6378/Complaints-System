d8b5f1ada4d8e5e4c533b6abb3bca59450846cb7
sammy6378 <samuel202mwangi@gmail.com>
2025-06-05 12:11:12 +0300

    Refactor AppModule for improved organization and clarity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,15 +8,40 @@ import { StatesModule } from './states/states.module';
 import { UserLogsModule } from './user-logs/user-logs.module';
 import { AdminLogsModule } from './admin-logs/admin-logs.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LogsModule } from './logs/logs.module';
 import { LoggerMiddleware } from './logger.middleware';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './auth/guards/at.guard';
+import { SeedModule } from './seed/seed.module';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { CacheableMemory } from 'cacheable';
+import { createKeyv, Keyv } from '@keyv/redis';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    // global cache
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (configService: ConfigService) => {
+        return {
+          ttl: 30000, // 30 seconds
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 30000, lruSize: 5000 }),
+            }),
+            createKeyv(configService.getOrThrow<string>('REDIS_URL')),
+          ],
+          Logger: true,
+        };
+      },
     }),
     UsersModule,
     AdminsModule,
@@ -28,12 +53,36 @@ import { LoggerMiddleware } from './logger.middleware';
     AdminLogsModule,
     DatabaseModule,
     LogsModule,
+    AuthModule,
+    SeedModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: CacheInterceptor, // global cache interceptor
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard, // protected routes
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('/users', '/logs'); // Apply logger middleware to all routes
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        '/users',
+        '/logs',
+        '/categories',
+        '/user_logs',
+        '/admin_logs',
+        '/subcategories',
+        '/states',
+        '/complaints',
+        '/admins',
+        '/auth',
+      ); // Apply logger middleware to all routes
   }
 }
