da2805be4289a69c59e7dc5ff3651cb207e35179
sammy6378 <samuel202mwangi@gmail.com>
2025-06-09 09:58:24 +0300

    Refactor UsersController to enhance role-based access control and standardize endpoint paths

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -7,12 +7,16 @@ import {
   Param,
   Delete,
   UseInterceptors,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, UserRole } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CacheInterceptor } from '@nestjs/cache-manager';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { AtGuard } from 'src/auth/guards/at.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('users')
 @UseInterceptors(CacheInterceptor)
@@ -20,27 +24,32 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Public()
-  @Post('create')
+  @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @Roles(UserRole.ADMIN)
+  @UseGuards(AtGuard, RolesGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @Roles(UserRole.ADMIN, UserRole.USER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
-  @Patch('/update/:id')
+  @Roles(UserRole.ADMIN, UserRole.USER)
+  @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete('/delete/:id')
+  @Roles(UserRole.ADMIN, UserRole.USER)
+  @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
