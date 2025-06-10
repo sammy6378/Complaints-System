25fbff6ed13b128e61eb56440f2c8c93daa3f70b
sammy6378 <samuel202mwangi@gmail.com>
2025-06-10 11:04:53 +0300

    Refactor MailService to streamline sendEmail method and remove unused mailTransporter function

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
-import path from 'path';
+import { join } from 'path';
 import * as ejs from 'ejs';
 import { sendEmailDto } from 'src/types/mailer';
 
 @Injectable()
 export class MailService {
   constructor(private readonly configService: ConfigService) {}
+
   // transporter
-  mailTransporter() {
+  async sendEmail(dto: sendEmailDto) {
+    const { recipients, subject, template, data } = dto;
+
     const transporter = nodemailer.createTransport({
       host: this.configService.get<string>('SMTP_HOST'),
       port: this.configService.get<number>('SMTP_PORT'),
@@ -22,27 +25,21 @@ export class MailService {
       logger: true,
       debug: true,
     });
-    return transporter;
-  }
 
-  //   template to be used to send emails more to one recipient
-  template(html: string, data: Record<string, string>) {
-    return html.replace(/%(\w*)%/g, function (m, key: string) {
-      return Object.prototype.hasOwnProperty.call(data, key) ? data[key] : '';
-    });
-  }
-
-  async sendEmail(dto: sendEmailDto) {
-    const { recipients, subject, template, data } = dto;
+    //   template to be used to send emails more to one recipient
+    // template(html: string, data: Record<string, string>) {
+    //   return html.replace(/%(\w*)%/g, function (m, key: string) {
+    //     return Object.prototype.hasOwnProperty.call(data, key) ? data[key] : '';
+    //   });
+    // }
 
     // const html = dto.placeholders
     //   ? this.template(dto.html, dto.placeholders)
     //   : dto.html;
 
-    const templatePath = path.join(__dirname, '../mails', template);
+    // const templatePath = path.join(__dirname, '../mails', template);
+    const templatePath = join(process.cwd(), 'views', template);
     const html: string = await ejs.renderFile(templatePath, data);
-
-    const transport = this.mailTransporter();
 
     const options: Mail.Options = {
       from: this.configService.get<string>('SMTP_EMAIL'),
@@ -51,15 +48,6 @@ export class MailService {
       html,
     };
 
-    try {
-      const res = await transport.sendMail(options);
-      if (!res) {
-        throw new Error('Unable to process the request');
-      }
-      return res;
-    } catch (error) {
-      console.log(error);
-      throw new Error('Unable to process the request');
-    }
+    await transporter.sendMail(options);
   }
 }
