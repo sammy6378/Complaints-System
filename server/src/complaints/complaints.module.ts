d02bfefc610fc1b1e4e6b83ac516b91ebe276dd5
sammy6378 <samuel202mwangi@gmail.com>
2025-06-10 11:04:16 +0300

    Add PoliciesGuard and CaslAbilityFactory to ComplaintsModule providers

--- server/src/complaints/complaints.module.ts
+++ server/src/complaints/complaints.module.ts
@@ -8,6 +8,8 @@ import { User } from 'src/users/entities/user.entity';
 import { Category } from 'src/categories/entities/category.entity';
 import { Subcategory } from 'src/subcategories/entities/subcategory.entity';
 import { State } from 'src/states/entities/state.entity';
+import { PoliciesGuard } from 'src/casl/guards/policies.guard';
+import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
 
 @Module({
   imports: [
@@ -15,6 +17,6 @@ import { State } from 'src/states/entities/state.entity';
     TypeOrmModule.forFeature([Complaint, User, Category, Subcategory, State]),
   ],
   controllers: [ComplaintsController],
-  providers: [ComplaintsService],
+  providers: [ComplaintsService, PoliciesGuard, CaslAbilityFactory],
 })
 export class ComplaintsModule {}
