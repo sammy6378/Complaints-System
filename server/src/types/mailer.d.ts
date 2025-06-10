54e53bc0546cf6e4acafa4c98997b4b890090e2c
sammy6378 <samuel202mwangi@gmail.com>
2025-06-10 11:05:12 +0300

    Define sendEmailDto type for email sending functionality

--- server/src/types/mailer.d.ts
+++ server/src/types/mailer.d.ts
@@ -1,8 +1,6 @@
-import { Address } from 'nodemailer/lib/mailer';
-
 export type sendEmailDto = {
-  recipients: Address[];
   subject: string;
+  recipients: string;
   template: string;
   data: Record<string, any>;
 };
