8f53877abb79b5ee6022d8d363358fb1f2950f99
sammy6378 <samuel202mwangi@gmail.com>
2025-06-10 11:02:23 +0300

    Refactor main.ts for improved application setup and documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,18 @@ import 'dotenv/config';
 import { ConfigService } from '@nestjs/config';
 import { AllExceptionsFilter } from './http-exceptions.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   // create a NestJS application instance
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ['error', 'warn', 'log'], // configure logger levels
   });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
 
   // add global validation pipe
   app.useGlobalPipes(new ValidationPipe());
@@ -27,9 +33,8 @@ async function bootstrap() {
   // swagger Ui
   const config = new DocumentBuilder()
     .setTitle('Complaints System')
-    .setDescription('API documentation for the Complaints System')
+    .setDescription('API documentation for ResolveIt Complaints System')
     .setVersion('1.0')
-    .addTag('complaint')
     .addBearerAuth({
       type: 'http',
       scheme: 'bearer',
@@ -38,7 +43,7 @@ async function bootstrap() {
     })
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/reference', app, documentFactory);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   // port configuration
   const configService = app.get(ConfigService);
@@ -58,7 +63,7 @@ async function bootstrap() {
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   };
 
-  app.enableCors({ corsOptions });
+  app.enableCors(corsOptions);
 
   await app.listen(PORT);
 
