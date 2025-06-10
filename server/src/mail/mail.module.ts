90555961569c74d87bbfd8cea59ba9dd6f98f40e
sammy6378 <samuel202mwangi@gmail.com>
2025-06-10 11:04:40 +0300

    Export MailService from MailModule for external usage

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -7,5 +7,6 @@ import { ConfigModule } from '@nestjs/config';
   imports: [ConfigModule],
   controllers: [MailController],
   providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
