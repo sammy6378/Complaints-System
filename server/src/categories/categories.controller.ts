00a2a941dd49bac05891b9cfd6651eeff497b564
sammy6378 <samuel202mwangi@gmail.com>
2025-06-12 21:52:12 +0300

    Enhance Categories module: add API documentation for CreateCategoryDto, update UpdateCategoryDto import, and apply guards in CategoriesController

--- server/src/categories/categories.controller.ts
+++ server/src/categories/categories.controller.ts
@@ -6,20 +6,31 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from 'src/users/dto/create-user.dto';
+import { AtGuard } from 'src/auth/guards/at.guard';
 
+@ApiTags('categories')
+@UseGuards(AtGuard, RolesGuard)
+@ApiBearerAuth()
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @Roles(UserRole.ADMIN)
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
   }
 
+  @Roles(UserRole.ADMIN)
   @Get()
   findAll() {
     return this.categoriesService.findAll();
@@ -30,6 +41,7 @@ export class CategoriesController {
     return this.categoriesService.findOne(id);
   }
 
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +50,7 @@ export class CategoriesController {
     return this.categoriesService.update(id, updateCategoryDto);
   }
 
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(id);
--- server/src/categories/dto/create-category.dto.ts
+++ server/src/categories/dto/create-category.dto.ts
@@ -1,9 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
+  @ApiProperty({
+    description: 'Name of the category',
+    example: 'Health',
+    type: String,
+    required: true,
+  })
   @IsString()
   category_name: string;
 
+  @ApiProperty({
+    description: 'Description of the category',
+    example: 'This category includes health-related topics.',
+    type: String,
+    required: true,
+  })
   @IsString()
   description: string;
 }
