ca1ca5fc9ba28240a06f67383407eac0c778914f
sammy6378 <samuel202mwangi@gmail.com>
2025-06-12 21:56:07 +0300

    Refactor sendEmailDto type: change recipients from string to Address[] for improved type safety.

--- server/src/types/mailer.d.ts
+++ server/src/types/mailer.d.ts
@@ -1,6 +1,8 @@
+import { Address } from 'nodemailer/lib/mailer';
+
 export type sendEmailDto = {
   subject: string;
-  recipients: string;
+  recipients: Address[];
   template: string;
   data: Record<string, any>;
 };
