dec0cd34d95ceb0b48f515cfc5c222493b5fc2d9
sammy6378 <samuel202mwangi@gmail.com>
2025-06-12 21:53:47 +0300

    Remove UpdateAdminLogDto: delete unused DTO for updating admin logs

--- server/src/admin-logs/dto/update-admin-log.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAdminLogDto } from './create-admin-log.dto';
-
-export class UpdateAdminLogDto extends PartialType(CreateAdminLogDto) {}
