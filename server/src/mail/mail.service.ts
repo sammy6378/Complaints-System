8fa6b10a3ba5f1df47f6ce8881439bed380a8707
sammy6378 <samuel202mwangi@gmail.com>
2025-06-14 20:15:14 +0300

    Refactor MailService to utilize MailerService for sending emails, improving code simplicity and maintainability.

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -1,53 +1,65 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as nodemailer from 'nodemailer';
-import Mail from 'nodemailer/lib/mailer';
-import { join } from 'path';
-import * as ejs from 'ejs';
 import { sendEmailDto } from 'src/types/mailer';
+
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MailService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly mailer: MailerService) {}
 
-  // transporter
-  async sendEmail(dto: sendEmailDto) {
-    const { recipients, subject, template, data } = dto;
-
-    const transporter = nodemailer.createTransport({
-      host: this.configService.get<string>('SMTP_HOST'),
-      port: this.configService.get<number>('SMTP_PORT'),
-      secure: false,
-      auth: {
-        user: this.configService.get<string>('SMTP_USER'),
-        pass: this.configService.get<string>('SMTP_PASS'),
-      },
-      logger: true,
-      debug: true,
-    });
-
-    //   template to be used to send emails more to one recipient
-    // template(html: string, data: Record<string, string>) {
-    //   return html.replace(/%(\w*)%/g, function (m, key: string) {
-    //     return Object.prototype.hasOwnProperty.call(data, key) ? data[key] : '';
-    //   });
-    // }
-
-    // const html = dto.placeholders
-    //   ? this.template(dto.html, dto.placeholders)
-    //   : dto.html;
-
-    // const templatePath = path.join(__dirname, '../mails', template);
-    const templatePath = join(process.cwd(), 'views', template);
-    const html: string = await ejs.renderFile(templatePath, data);
-
-    const options: Mail.Options = {
-      from: this.configService.get<string>('SMTP_EMAIL'),
+  async sendEmail({ recipients, subject, template, data }: sendEmailDto) {
+    await this.mailer.sendMail({
       to: recipients,
       subject,
-      html,
-    };
-
-    await transporter.sendMail(options);
+      template, // no .ejs extension
+      context: data,
+      // `from` comes from the defaults we set in MailerModule
+    });
   }
 }
+
+// @Injectable()
+// export class MailService {
+//   constructor(private readonly configService: ConfigService) {}
+
+//   // transporter
+//   async sendEmail(dto: sendEmailDto) {
+//     const { recipients, subject, template, data } = dto;
+
+//     const transporter = nodemailer.createTransport({
+//       host: this.configService.get<string>('SMTP_HOST'),
+//       port: this.configService.get<number>('SMTP_PORT'),
+//       secure: false,
+//       auth: {
+//         user: this.configService.get<string>('SMTP_USER'),
+//         pass: this.configService.get<string>('SMTP_PASS'),
+//       },
+//       logger: true,
+//       debug: true,
+//     });
+
+//     //   template to be used to send emails more to one recipient
+//     // template(html: string, data: Record<string, string>) {
+//     //   return html.replace(/%(\w*)%/g, function (m, key: string) {
+//     //     return Object.prototype.hasOwnProperty.call(data, key) ? data[key] : '';
+//     //   });
+//     // }
+
+//     // const html = dto.placeholders
+//     //   ? this.template(dto.html, dto.placeholders)
+//     //   : dto.html;
+
+//     // const templatePath = path.join(__dirname, '../mails', template);
+//     const templatePath = join(process.cwd(), 'views', template);
+//     const html: string = await ejs.renderFile(templatePath, data);
+
+//     const options: Mail.Options = {
+//       from: this.configService.get<string>('SMTP_EMAIL'),
+//       to: recipients,
+//       subject,
+//       html,
+//     };
+
+//     await transporter.sendMail(options);
+//   }
+// }
