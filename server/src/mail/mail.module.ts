57109b4a8e6f1e6c3cf7fc8a948ee0ec64e313d6
sammy6378 <samuel202mwangi@gmail.com>
2025-06-14 20:14:55 +0300

    Refactor MailModule to use async configuration for MailerModule with improved SMTP settings

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -1,12 +1,39 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.getOrThrow<string>('SMTP_HOST'),
+          port: config.getOrThrow<number>('SMTP_PORT'),
+          secure: config.getOrThrow<boolean>('SMTP_SECURE'),
+          auth: {
+            user: config.getOrThrow<string>('SMTP_USER'),
+            pass: config.getOrThrow<string>('SMTP_PASS'),
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${config.getOrThrow<string>('SMTP_EMAIL')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'), // mail/templates/…
+          adapter: new EjsAdapter(),
+          options: { strict: true },
+        },
+      }),
+    }),
+  ],
   controllers: [MailController],
   providers: [MailService],
-  exports: [MailService],
+  exports: [MailService], // so other modules can inject it
 })
 export class MailModule {}
