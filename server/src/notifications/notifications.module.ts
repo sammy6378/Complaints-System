4146db29870921d0ac781ba59f77159b4738f099
sammy6378 <samuel202mwangi@gmail.com>
2025-06-14 20:23:06 +0300

    Add MailService to NotificationsModule providers

--- server/src/notifications/notifications.module.ts
+++ server/src/notifications/notifications.module.ts
@@ -5,10 +5,11 @@ import { DatabaseModule } from 'src/database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
 import { User } from 'src/users/entities/user.entity';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   imports: [DatabaseModule, TypeOrmModule.forFeature([Notification, User])],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, MailService],
 })
 export class NotificationsModule {}
